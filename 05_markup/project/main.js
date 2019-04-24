80084961a8cf80edaaf40c124ef9e6255fcc394b
filipovichjenya <a.hz@list.ru>
2019-04-24 12:35:55 +0300

    dz_markup

--- 05_markup/project/main.js
+++ 05_markup/project/main.js
@@ -1,31 +1,27 @@
 const sections = document.querySelectorAll('section');
 const turret = document.getElementById('tank_turret');
+const tank = document.querySelector(".art-tank-fw");
 
 for (const section of sections) {
-  section.addEventListener('mouseenter', e => {
-    const turretDirection = e.target.dataset.turretDirection;
-    turret.classList.add(`tank_turret__${turretDirection}`);
-  });
+    section.addEventListener('mouseenter', e => {
+        const turretDirection = e.target.dataset.turretDirection;
+        turret.classList.add(`tank_turret__${turretDirection}`);
+        console.log(turretDirection);
+    });
 
-  section.addEventListener('mouseleave', e => {
-    const turretDirection = e.target.dataset.turretDirection;
-    turret.classList.remove(`tank_turret__${turretDirection}`);
-  });
+    section.addEventListener('mouseleave', e => {
+        const turretDirection = e.target.dataset.turretDirection;
+        turret.classList.remove(`tank_turret__${turretDirection}`);
+    });
 }
 
 sections[0].addEventListener('click', e => {
-  document.body.className = 'toggle';
+    document.body.className = 'toggle';
 });
 
 
-
-
-
-const tank = document.getElementsByClassName('art-tank-fw parallax-ultra-fore')[0];
-let lastPos = 0;
-let lastScrollTop = 0;
-sections[0].addEventListener('scroll', (e)=>{
-    curentTop
-    tank.style.transform = `translateX(${lastPos}px)`;
-    lastPos += 2;
+sections[0].addEventListener("scroll", e => {
+    let pos = sections[0].scrollTop / 2 - 350;
+    tank.style.transform = `translateX(${pos}px) translateY(${pos / 2}px)`; 
+    // translateY для видимости текста за танком
 })
